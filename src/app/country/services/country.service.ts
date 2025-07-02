e97ecbe0b36943c6064ef2e32e16ff8de7e11ed2
Lionel Cancellieri <lio.cancellieri@gmail.com>
2025-07-01 22:17:15 -0300

    Updated: Mostrar diferentes estados al usuario, sección 10: PC

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, throwError } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -33,6 +33,7 @@ export class CountryService {
     return this.http.get<RESTCountry[]>(url)
       .pipe(
         map((restCountries) => CountryMapper.mapRestCountryArrayToCountryArray(restCountries) ),
+        delay(2000),
         catchError( (error) => {
           console.log('Error fetching', error);
           return throwError(
